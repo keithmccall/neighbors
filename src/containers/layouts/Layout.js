6fca5e79d4d0f8db34c376c84a2fd4b45f74704c
keithmccall_acadaca <keith.mccall@acadaca.com>
2018-11-19 17:34:45 -0500

    quick push

---
+++ src/containers/layouts/Layout.js
@@ -0,0 +1,15 @@
+import React, { Component, Fragment } from "react";
+import Header from "../header/Header";
+import Footer from "../footer/Footer";
+//
+export default class Layout extends Component {
+    render() {
+        return (
+            <Fragment>
+                <Header/>
+                {this.props.children}
+                <Footer/>
+            </Fragment>
+        );
+    }
+}
