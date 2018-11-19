60148b591ff6de845533946739efc721531c9d5b
keithmccall_acadaca <keith.mccall@acadaca.com>
2018-11-19 18:19:26 -0500

    quick push

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Layout from "./containers/layouts/Layout";
 
 class App extends Component {
     render() {
         return (
-            <div >
-                flat
-            </div>
+           <Layout>
+               <main><p>main</p></main>
+           </Layout>
         );
     }
 }
