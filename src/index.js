214a9c87a48b2e22fb4a9222b5f8c3179314cd4b
keithmccall_acadaca <keith.mccall@acadaca.com>
2018-11-26 13:06:46 -0500

    Added node-sass

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './style/css/index.css'
+import './styles/App.scss';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
